45aeb25dc41a405ef75c4b491b97537a838599fe
santiago <arenas4545a@gmail.com>
2025-04-29 18:23:18 -0500

    Primer comit

---
+++ src/api/apiRickAndMorti.js
@@ -0,0 +1,9 @@
+export const BASE_URL = 'https://rickandmortyapi.com/api/character/'
+
+export async function fetchCharacters(page = 1){
+    try {
+        let url = `${BASE_URL}`
+    } catch (error) {
+        
+    }
+}
