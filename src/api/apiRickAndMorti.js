9dca510231ae98d8dca75eb882f339954a686c9c
santiago <arenas4545a@gmail.com>
2025-04-29 18:40:08 -0500

    avanzando

--- src/api/apiRickAndMorti.js
+++ src/api/apiRickAndMorti.js
@@ -2,8 +2,11 @@ export const BASE_URL = 'https://rickandmortyapi.com/api/character/'
 
 export async function fetchCharacters(page = 1){
     try {
-        let url = `${BASE_URL}`
+        let url = `${BASE_URL}?page=${page}`
+        const response = await fetch(url);
+        const data = await response.json();
+        return data;
     } catch (error) {
-        
+        console.error('Error en fetch de characters', error)
     }
 }
